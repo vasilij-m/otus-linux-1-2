77b42d3eba3b6c6ce75f30b115bbc5490aa99a82
vasilij-m <basil.moiseyev@yandex.ru>
2020-02-09 23:47:55 +0300

    Работа с mdadm

--- task-1/raid10.sh
+++ task-1/raid10.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 mdadm --zero-superblock --force /dev/sd{b,c,d,e}
-sudo mdadm --create /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e}
+mdadm --create --verbose /dev/md0 -l 10 -n 4 /dev/sd{b,c,d,e}
 echo "DEVICE partitions" | sudo tee -a /etc/mdadm.conf
-mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' | sudo tee -a /etc/mdadm.conf
-sudo parted -s /dev/md0 mklabel gpt
+mdadm --detail --scan | awk '/ARRAY/ {print}' | tee -a /etc/mdadm.conf
+parted -s /dev/md0 mklabel gpt
 parted /dev/md0 mkpart primary ext4 0% 20%
 parted /dev/md0 mkpart primary ext4 20% 40%
 parted /dev/md0 mkpart primary ext4 40% 60%
 parted /dev/md0 mkpart primary ext4 60% 80%
 parted /dev/md0 mkpart primary ext4 80% 100%
-for i in $(seq 1 5); do sudo mkfs.ext4 /dev/md0p$i; done
+for i in $(seq 1 5); do mkfs.ext4 /dev/md0p$i; done
 mkdir -p /raid/part{1,2,3,4,5}
-for i in $(seq 1 5); do sudo mount /dev/md0p$i /raid/part$i; done
-
+for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
